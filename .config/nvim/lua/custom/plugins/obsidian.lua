07f460dbeaf9739008dd2e4305c19a0edfd052ed
andomm <antton.anttila01@gmail.com>
2024-04-11 16:46:02 +0300

    note id is the same as the title

--- .config/nvim/lua/custom/plugins/obsidian.lua
+++ .config/nvim/lua/custom/plugins/obsidian.lua
@@ -3,18 +3,8 @@ return {
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = 'markdown',
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
   dependencies = {
-    -- Required.
     'nvim-lua/plenary.nvim',
-
-    -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
@@ -23,7 +13,14 @@ return {
         path = '~/my-notes/',
       },
     },
-
-    -- see below for full list of options 👇
+    daily_notes = {
+      folder = '~/my-notes/daily-notes/',
+      date_format = '%d-%m-%Y',
+    },
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      return tostring(title)
+    end,
   },
 }
