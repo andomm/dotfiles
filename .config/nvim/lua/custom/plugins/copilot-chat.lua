51e8fb3762b2fea5ddc449d953cd159b41c09235
andomm <antton.anttila01@gmail.com>
2025-02-28 09:18:41 +0200

    copilot-chat to not lazy load

--- .config/nvim/lua/custom/plugins/copilot-chat.lua
+++ .config/nvim/lua/custom/plugins/copilot-chat.lua
@@ -2,6 +2,7 @@ return {
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     branch = 'main',
+    lazy = false,
     dependencies = {
       { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
       { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
