351010e711d6fac3bf76704232b5f319daeef5e1
andomm <antton.anttila01@gmail.com>
2025-05-13 08:44:06 +0300

    Use ruff_format, dont use pyright for type analysis

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -453,7 +453,16 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         html = {},
-        pyright = {},
+        ruff = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                ignore = { '*' },
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -533,7 +542,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff', 'black', 'isort' },
+        python = { 'ruff_format' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
