4d11dc4c1da74f1c395c54b83e4bd4118e05bc80
andomm <antton.anttila01@gmail.com>
2025-05-15 19:47:40 +0300

    pyright works again

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -454,15 +454,7 @@ require('lazy').setup({
         gopls = {},
         html = {},
         ruff = {},
-        pyright = {
-          settings = {
-            python = {
-              analysis = {
-                ignore = { '*' },
-              },
-            },
-          },
-        },
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -508,6 +500,7 @@ require('lazy').setup({
         'isort',
         'mypy',
         'pylint',
+        'ruff',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
