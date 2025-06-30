c632350fa8ec4a840b510d30ed5039d02fec44d4
andomm <antton.anttila01@gmail.com>
2025-06-30 08:57:48 +0300

    use tmux sessionizer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -58,7 +58,7 @@ vim.opt.wrap = false
 
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-new-window<CR>')
+vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>k', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>cprev<CR>zz')
